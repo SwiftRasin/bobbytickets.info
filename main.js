08f19efdee689520e5eab5651284e798e3ea99dd
Cartoon_64 <swiftrasin64@gmail.com>
2024-09-23 22:29:48 -0400

    bobbyco

--- main.js
+++ main.js
@@ -61,18 +61,18 @@ var bobby_tickets = {
     "series4b": {
         "available": true,
 
-        "nerd bobby": makeTicket("nerd bobby", "4b", "nerd bobby", 12, true, 2),
-        "caffeinated bobby": makeTicket("caffeinated bobby", "4b", "caffeinated bobby", 12, true, 2),
-        "hardworker bobby": makeTicket("hardworker bobby", "4b", "hardworker bobby", 12, true, 3),
-        "nine-to-five": makeTicket("nine-to-five", "4b", "nine-to-five", 12, true, 3),
-        "very happy bobby": makeTicket("very happy bobby", "4b", "very happy bobby", 6, true, 5),
-        "bobbytsu ツ": makeTicket("bobbytsu ツ", "4b", "bobbytsu ツ", 12, true, 1),
-        "THE OPPOSITION": makeTicket("THE OPPOSITION", "4b", "THE OPPOSITION", 6, true, 5),
-        "what even is this": makeTicket("what even is this", "4b", "what even is this", 12, true, 1),
-        "emo bobby": makeTicket("emo bobby", "4b", "emo bobby", 12, true, 2),
-        "disgusted bobby": makeTicket("disgusted bobby", "4b", "disgusted bobby", 12, true, 2),
-        "snowman bobby": makeTicket("snowman bobby", "4b", "snowman bobby", 12, true, 3),
-        "dizzy bobby": makeTicket("dizzy bobby", "4b", "dizzy bobby", 12, true, 2),
+        "nerd bobby": makeTicket("nerd bobby", "4b", "nerd bobby", 36, true, 2),
+        "caffeinated bobby": makeTicket("caffeinated bobby", "4b", "caffeinated bobby", 36, true, 2),
+        "hardworker bobby": makeTicket("hardworker bobby", "4b", "hardworker bobby", 36, true, 3),
+        "nine-to-five": makeTicket("nine-to-five", "4b", "nine-to-five", 36, true, 3),
+        "very happy bobby": makeTicket("very happy bobby", "4b", "very happy bobby", 18, true, 5),
+        "bobbytsu ツ": makeTicket("bobbytsu ツ", "4b", "bobbytsu ツ", 36, true, 1),
+        "THE OPPOSITION": makeTicket("THE OPPOSITION", "4b", "THE OPPOSITION", 18, true, 5),
+        "what even is this": makeTicket("what even is this", "4b", "what even is this", 36, true, 1),
+        "emo bobby": makeTicket("emo bobby", "4b", "emo bobby", 36, true, 2),
+        "disgusted bobby": makeTicket("disgusted bobby", "4b", "disgusted bobby", 36, true, 2),
+        "snowman bobby": makeTicket("snowman bobby", "4b", "snowman bobby", 36, true, 3),
+        "dizzy bobby": makeTicket("dizzy bobby", "4b", "dizzy bobby", 36, true, 2),
     },
 }
 
@@ -182,6 +182,7 @@ function setup() {
 
 function draw()
 {
+    
     for (var i = 0; i < alltickets.length; i++)
     {
         if (alltickets[i].children[0].checked)
