4a2456d54a56d6ffb48ef937dde54c2f892978da
Cartoon_64 <swiftrasin64@gmail.com>
2024-11-03 23:03:07 -0500

    QUICK FIX

--- util.js
+++ util.js
@@ -3,5 +3,7 @@ function subpage(dir)
     if (dir !== "") {
         dir = "/" + dir;
     }
+    if (window.location.href.includes("/bobbytickets.info/"))
+        dir += "bobbytickets.info/";
     window.location.href = window.location.origin + dir;
 }
