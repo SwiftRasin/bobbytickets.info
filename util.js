becaa4aac1e6ef517c2c33a19f1e186d806e0514
Cartoon_64 <swiftrasin64@gmail.com>
2024-11-03 23:05:32 -0500

    EVEN QUICKER FIX

--- util.js
+++ util.js
@@ -2,8 +2,14 @@ function subpage(dir)
 {
     if (dir !== "") {
         dir = "/" + dir;
+        if (window.location.href.includes("/bobbytickets.info/"))
+            dir = "/bobbytickets.info" + dir;
+        window.location.href = window.location.origin + dir;
     }
-    if (window.location.href.includes("/bobbytickets.info/"))
-        dir += "bobbytickets.info/";
-    window.location.href = window.location.origin + dir;
+    else
+    {
+        if (window.location.href.includes("/bobbytickets.info/"))
+            dir = "/bobbytickets.info/";
+        window.location.href = window.location.origin + dir;
+    }
 }
